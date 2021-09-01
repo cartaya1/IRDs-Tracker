9a11fe7dd845d9ae112a2cc6c7964fcfef09e55c
Luis Cartaya <cartaya1@msn.com>
2021-08-31 23:46:17 -0400

    new Schema file TEST

--- server/index test.js
+++ server/index test.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express ()
 const mongoose = require('mongoose');
-const dailyModel = require('./models/daily');
+const masterModel = require('./models/master');
 
 ///Database Conection
 mongoose.connect("mongodb://localhost:27017/IRD_db?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
 {useNewUrlParser: true}
 );
 
-app.get("/daily", async (req, res) => {
-    dailyModel.find ({}, (err, result) => {
+app.get("/master", async (req, res) => {
+    masterModel.find ({}, (err, result) => {
         if (err){
             res.send(err)
         } else {
