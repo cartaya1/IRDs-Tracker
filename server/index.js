2c57a0398f10b8fb4868a49cad6a6e06f13dc30f
Luis Cartaya <cartaya1@msn.com>
2021-08-31 22:16:57 -0400

    new Schema file

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const techModel = require('./models/Techs');
 
 ///Database Conection
-mongoose.connect("mongodb://localhost:27017/IRD-db?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
+mongoose.connect("mongodb://localhost:27017/IRD_db?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
 {useNewUrlParser: true}
 );
 
-app.get("/data", async (req, res) => {
+app.get("/info", async (req, res) => {
     techModel.find ({}, (err, result) => {
         if (err){
             res.send(err)
@@ -17,8 +17,6 @@ app.get("/data", async (req, res) => {
         }
     })
 })
-
-
 
 app.listen (3001, ()=>{
     console.log('test: you are conected')
---
+++ server/models/ohr.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const ohrSchema = new mongoose.Schema({
+
+TECH: {
+    type: String,
+    required: true,
+},
+LOCATION: {
+    type: String,
+    required: true,
+},
+ISS_DATE: {
+    type: Date,
+    required: true,
+},
+REC_DATE: {
+    type: Date,
+    required: true,
+},
+MODEL: {
+    type: String,
+    required: true,
+},
+SERIAL: {
+    type: String,
+    required: true,
+},
+CARD: {
+    type: String,
+    required: true,
+},
+AGE: {
+    type: INT,
+    required: true,
+},
+
+})
+
+const ohrModel = mongoose.model('ohr', ohrSchema)
+
+module.exports = ohrModel
